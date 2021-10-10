d980bdb70a1d0ed4a05eeeeb53ebd40e51742921
Rakshith Raj G P <rakshithraj.gp11@gmail.com>
2021-10-10 06:01:34 +0530

    Scaffolding flutter App Widgets: Scaffold,AppBar,Image

--- 1.Basics/1.Creating First Flutter App/lib/main.dart
+++ 1.Basics/1.Creating First Flutter App/lib/main.dart
@@ -6,12 +6,23 @@ import 'package:flutter/material.dart';
 
 //We can reformat the code by dart format by right-click
 
+//Scaffold() widget is provides us with the most common layouts in the basic material design, like an appBar
+//single quotes are preferred to use
+//To wrap widget within another widget we can plcae cursor on the widget to be wrapped and press alt + enter
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text(
-          "Hello World!",
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey[100],
+        appBar: AppBar(
+          title: Text('I am Rich'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image(
+            image: NetworkImage(
+                'https://images.unsplash.com/photo-1633603063175-e9ab2820ddbb?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1112&q=80'),
+          ),
         ),
       ),
     ),
